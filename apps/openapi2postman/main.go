c4f58a0e70688cfbecf24a0b610eebed477358a7
grokify <johncwang@gmail.com>
2019-05-02 09:30:49 -0700

    move example to `openapi2postman` CLI app

---
+++ apps/openapi2postman/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/grokify/swaggman"
+	"github.com/grokify/swaggman/postman2"
+	"github.com/jessevdk/go-flags"
+)
+
+// Convert yaml2json: https://github.com/bronze1man/yaml2json ... yaml2json_darwin_amd64
+
+type Options struct {
+	PostmanBase string `short:"b" long:"base" description:"Basic Postman File"`
+	Postman     string `short:"p" long:"postman" description:"Output Postman File" required:"true"`
+	Swagger     string `short:"s" long:"swagger" description:"Input Swagger File" required:"true"`
+}
+
+func main() {
+	var opts Options
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg := swaggman.Configuration{
+		PostmanURLHostname: "{{RINGCENTRAL_SERVER_HOSTNAME}}",
+		PostmanHeaders: []postman2.Header{{
+			Key:   "Authorization",
+			Value: "Bearer {{my_access_token}}"}}}
+
+	conv := swaggman.NewConverter(cfg)
+
+	merge := true
+
+	if merge {
+		err = conv.MergeConvert(opts.Swagger, opts.PostmanBase, opts.Postman)
+	} else {
+		err = conv.Convert(opts.Swagger, opts.Postman)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("Wrote %v\n", opts.Postman)
+	}
+
+	fmt.Println("DONE")
+}
