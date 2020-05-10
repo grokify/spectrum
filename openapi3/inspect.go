555d71de2eea15361790b91ca1ed320f67c45476
John Wang <johncwang@gmail.com>
2020-05-10 00:53:33 -0700

    add WIP extension name finder

--- openapi3/inspect.go
+++ openapi3/inspect.go
@@ -6,6 +6,25 @@ import (
 	oas3 "github.com/getkin/kin-openapi/openapi3"
 )
 
+// SpecExtensionNames is not complete yet.
+func SpecExtensionNames(spec *oas3.Swagger) map[string]int {
+	extNames := map[string]int{}
+	for _, schRef := range spec.Components.Schemas {
+		if schRef.Value == nil {
+			continue
+		}
+		sch := schRef.Value
+		for extName := range sch.ExtensionProps.Extensions {
+			count, ok := extNames[extName]
+			if !ok {
+				extNames[extName] = 1
+			}
+			extNames[extName] = count + 1
+		}
+	}
+	return extNames
+}
+
 func SpecHasComponentSchema(spec *oas3.Swagger, name string, lowerCaseMatch bool) bool {
 	name = strings.TrimSpace(name)
 	if lowerCaseMatch {
