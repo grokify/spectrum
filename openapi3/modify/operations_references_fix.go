e2892e91c06ba5b4466d5a3f7453ed4431d83643
John Wang <johncwang@gmail.com>
2020-10-13 04:24:06 -0700

    feat: openapi3: add SpecOperationsFixResponseReferences()

---
+++ openapi3/modify/operations_references_fix.go
@@ -0,0 +1,51 @@
+package modify
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	oas3 "github.com/getkin/kin-openapi/openapi3"
+	"github.com/grokify/swaggman/openapi3"
+)
+
+const oas3ComponentsSchemasBase = "#/components/schemas"
+
+var rxOAS2RefDefinition = regexp.MustCompile(`#/definitions/(.*)`)
+
+func SpecOperationsFixResponseReferences(spec *oas3.Swagger) []*openapi3.OperationMeta {
+	errorOperations := []*openapi3.OperationMeta{}
+	openapi3.VisitOperations(spec, func(path, method string, op *oas3.Operation) {
+		if op == nil {
+			return
+		}
+		for resCode, resRef := range op.Responses {
+			if strings.Index(resRef.Ref, "#/definitions/") == 0 {
+				m := rxOAS2RefDefinition.FindStringSubmatch(resRef.Ref)
+				if len(m) > 0 {
+					resRefOrig := resRef.Ref
+					resRef.Ref = oas3ComponentsSchemasBase + "/" + m[1]
+					om := openapi3.OperationToMeta(path, method, op)
+					om.MetaNotes = append(om.MetaNotes,
+						fmt.Sprintf("E_BAD_RESPONSE_REF_OAS2_DEF [%s] type[%s]", resCode, resRefOrig))
+					errorOperations = append(errorOperations, &om)
+				}
+			}
+		}
+	})
+	return errorOperations
+}
+
+func FixFile(input, output string, prefix, indent string, validateOutput bool) (*oas3.Swagger, []*openapi3.OperationMeta, error) {
+	spec, err := openapi3.ReadFile(input, false)
+	if err != nil {
+		return spec, []*openapi3.OperationMeta{}, err
+	}
+	errs := SpecOperationsFixResponseReferences(spec)
+	output = strings.TrimSpace(output)
+	if len(output) > 0 {
+		smore := openapi3.SpecMore{Spec: spec}
+		return spec, errs, smore.WriteFileJSON(output, 0644, prefix, indent)
+	}
+	return spec, errs, err
+}
