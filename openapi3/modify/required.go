b2d3ca4c28932bbca94567000fc205b945bcda9f
John Wang <johncwang@gmail.com>
2021-01-19 07:51:56 -0800

    feat: openapi3/modify: add SpecSetSchemaPropertiesOptional()

---
+++ openapi3/modify/required.go
@@ -0,0 +1,34 @@
+package modify
+
+import (
+	"regexp"
+
+	"github.com/grokify/simplego/type/stringsutil"
+
+	oas3 "github.com/getkin/kin-openapi/openapi3"
+)
+
+var rxOptionalDefault = regexp.MustCompile(`(?i)\boptional\b`)
+
+func SpecSetSchemaPropertiesOptional(spec *oas3.Swagger, rxOptional *regexp.Regexp) {
+	if rxOptional == nil {
+		return
+	}
+	for _, schemaRef := range spec.Components.Schemas {
+		if len(schemaRef.Ref) == 0 && schemaRef.Value != nil {
+			required := []string{}
+			for propName, propRef := range schemaRef.Value.Properties {
+				if len(propRef.Ref) == 0 && propRef.Value != nil {
+					if len(propRef.Value.Description) > 0 &&
+						!rxOptional.MatchString(propRef.Value.Description) {
+						required = append(required, propName)
+					}
+				}
+			}
+			if len(required) > 1 {
+				required = stringsutil.SliceCondenseSpace(required, true, true)
+			}
+			schemaRef.Value.Required = required
+		}
+	}
+}
