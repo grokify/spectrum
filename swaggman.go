570f19360ef3119cc8248efefe149515a28d7953
John Wang <johncwang@gmail.com>
2021-05-03 23:53:52 -0700

    feat: swaggmaan cmd: add XSLX output option

--- swaggman.go
+++ swaggman.go
@@ -14,10 +14,11 @@ import (
 // Convert yaml2json: https://github.com/bronze1man/yaml2json ... yaml2json_darwin_amd64
 
 type Options struct {
+	OpenAPIFile string `short:"O" long:"openapiFile" description:"Input Swagger File" required:"true"`
 	Config      string `short:"C" long:"config" description:"Swaggman Config File"`
 	PostmanBase string `short:"B" long:"basePostmanFile" description:"Basic Postman File"`
-	Postman     string `short:"P" long:"postmanFile" description:"Output Postman File" required:"true"`
-	OpenAPIFile string `short:"O" long:"openapiFile" description:"Input Swagger File" required:"true"`
+	Postman     string `short:"P" long:"postmanFile" description:"Output Postman File"`
+	XLSXFile    string `short:"X" long:"xlsxFile" description:"Output XLSX File"`
 }
 
 func (opts *Options) TrimSpace() {
@@ -34,34 +35,44 @@ func main() {
 		log.Fatal(err)
 	}
 
-	cfg3 := openapi3postman2.Configuration{}
-
-	if len(opts.Config) > 0 {
-		cfg3, err = openapi3postman2.ConfigurationReadFile(opts.Config)
-		if err != nil {
-			errors.Wrap(err, "openapi3postman2.ConfigurationReadFile")
-			log.Fatal(err)
-		}
-	}
-
 	spec, err := openapi3.ReadFile(opts.OpenAPIFile, true)
 	if err != nil {
 		log.Fatal(err)
 	}
-	conv := openapi3postman2.Converter{
-		Configuration: cfg3,
-		OpenAPISpec:   spec}
 
-	err = conv.MergeConvert(
-		opts.OpenAPIFile,
-		opts.PostmanBase,
-		opts.Postman)
+	if len(opts.Postman) > 0 {
+		cfg3 := openapi3postman2.Configuration{}
 
-	if err != nil {
-		errors.Wrap(err, "swaggman.main << conv.MergeConvert")
-		log.Fatal(err)
+		if len(opts.Config) > 0 {
+			cfg3, err = openapi3postman2.ConfigurationReadFile(opts.Config)
+			if err != nil {
+				errors.Wrap(err, "openapi3postman2.ConfigurationReadFile")
+				log.Fatal(err)
+			}
+		}
+
+		conv := openapi3postman2.Converter{
+			Configuration: cfg3,
+			OpenAPISpec:   spec}
+
+		err = conv.MergeConvert(
+			opts.OpenAPIFile,
+			opts.PostmanBase,
+			opts.Postman)
+
+		if err != nil {
+			errors.Wrap(err, "swaggman.main << conv.MergeConvert")
+			log.Fatal(err)
+		}
+
+		fmt.Printf("wrote Postman collection [%s]\n", opts.Postman)
 	}
-
-	fmt.Printf("wrote Postman collection [%s]\n", opts.Postman)
+	if len(opts.XLSXFile) > 0 {
+		sm := openapi3.SpecMore{Spec: spec}
+		err := sm.WriteFileXLSX(opts.XLSXFile, nil, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 	fmt.Println("DONE")
 }
