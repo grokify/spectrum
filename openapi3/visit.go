a6d80876bafe21b2cb2c6b9cba95f8d1b13f5f03
John Wang <johncwang@gmail.com>
2021-07-13 00:17:14 -0700

    streamline: openapi3: remove old code

--- openapi3/visit.go
+++ openapi3/visit.go
@@ -100,34 +100,5 @@ func VisitOperationsPathItem(path string, pathItem *oas3.PathItem, visitOp func(
 func VisitOperations(spec *Spec, visitOp func(path, method string, op *oas3.Operation)) {
 	for path, pathItem := range spec.Paths {
 		VisitOperationsPathItem(path, pathItem, visitOp)
-		/*
-			if pathItem.Connect != nil {
-				visitOp(path, http.MethodConnect, pathItem.Connect)
-			}
-			if pathItem.Delete != nil {
-				visitOp(path, http.MethodDelete, pathItem.Delete)
-			}
-			if pathItem.Get != nil {
-				visitOp(path, http.MethodGet, pathItem.Get)
-			}
-			if pathItem.Head != nil {
-				visitOp(path, http.MethodHead, pathItem.Head)
-			}
-			if pathItem.Options != nil {
-				visitOp(path, http.MethodOptions, pathItem.Options)
-			}
-			if pathItem.Patch != nil {
-				visitOp(path, http.MethodPatch, pathItem.Patch)
-			}
-			if pathItem.Post != nil {
-				visitOp(path, http.MethodPost, pathItem.Post)
-			}
-			if pathItem.Put != nil {
-				visitOp(path, http.MethodPut, pathItem.Put)
-			}
-			if pathItem.Trace != nil {
-				visitOp(path, http.MethodTrace, pathItem.Trace)
-			}
-		*/
 	}
 }
