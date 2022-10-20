6109af6f745b7ede4a4fe10896cfe74d06e2e39a
John Wang <johncwang@gmail.com>
2022-10-20 05:07:41 -0700

    feat: `openapi3`: add `ReadSchemaFile()`

--- openapi3/schemas.go
+++ openapi3/schemas.go
@@ -1,8 +1,10 @@
 package openapi3
 
 import (
+	"os"
 	"strings"
 
+	oas3 "github.com/getkin/kin-openapi/openapi3"
 	"github.com/grokify/mogo/encoding/jsonutil"
 )
 
@@ -20,4 +22,14 @@ func SchemaPointerExpand(prefix, schemaName string) string {
 		pointer = prefix + pointer
 	}
 	return pointer
+}
+
+func ReadSchemaFile(filename string) (*oas3.Schema, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	sch := oas3.NewSchema()
+	err = sch.UnmarshalJSON(data)
+	return sch, err
 }
