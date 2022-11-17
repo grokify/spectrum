2c6b3285f2362f97ee80136a261d85804832ec50
grokify <grokify@gmail.com>
2022-11-17 04:44:28 -0800

    feat: `openapi3.SpecMore`: add `SchemasCopyJSONPointer()`, `SchemasCopyOperation()`, and `SchemasCopySchemaRef()`; enhance `SpecMore.ExportByTag()`

--- openapi3/spec_more_export.go
+++ openapi3/spec_more_export.go
@@ -1,5 +1,14 @@
 package openapi3
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	oas3 "github.com/getkin/kin-openapi/openapi3"
+)
+
+// ExportByTags creates individual specs by tag.
 func (sm *SpecMore) ExportByTags() (map[string]*Spec, error) {
 	specs := map[string]*Spec{}
 	if sm.Spec == nil {
@@ -19,14 +28,20 @@ func (sm *SpecMore) ExportByTags() (map[string]*Spec, error) {
 	return specs, nil
 }
 
+// ExportByTag creates an individual specs for one tag.
 func (sm *SpecMore) ExportByTag(tag string) (*Spec, error) {
 	if (sm.Spec) == nil {
 		return nil, ErrSpecNotSet
 	}
 	oms := sm.OperationMetas([]string{tag})
-	tagSpec := &Spec{}
 	if len(oms) == 0 {
 		return nil, nil
+	}
+	tagSpec := &Spec{
+		Components: oas3.Components{
+			ExtensionProps:  sm.Spec.Components.ExtensionProps,
+			SecuritySchemes: sm.Spec.Components.SecuritySchemes,
+		},
 	}
 	for _, om := range oms {
 		op, err := sm.OperationByPathMethod(om.Path, om.Method)
@@ -36,6 +51,93 @@ func (sm *SpecMore) ExportByTag(tag string) (*Spec, error) {
 			continue
 		}
 		tagSpec.AddOperation(om.Path, om.Method, op)
+		err = sm.SchemasCopyOperation(tagSpec, op)
+		if err != nil {
+			return nil, err
+		}
 	}
-	return Copy(tagSpec)
+	tagSm := SpecMore{Spec: tagSpec}
+	return tagSm.Clone()
+}
+
+func (sm *SpecMore) SchemasCopyOperation(destSpec *Spec, op *oas3.Operation) error {
+	if sm.Spec == nil || destSpec == nil || op == nil {
+		return errors.New("source spec, dest spec, op cannot be nil")
+	}
+	omr := OperationMore{Operation: op}
+	refs := omr.JSONPointers()
+	for refJSONPointer := range refs {
+		ptr, err := ParseJSONPointer(refJSONPointer)
+		if err != nil {
+			return err
+		}
+		paramName, isParam := ptr.IsTopParameter()
+		if isParam {
+			if paramRef, ok := sm.Spec.Components.Parameters[paramName]; ok {
+				if destSpec.Components.Parameters == nil {
+					destSpec.Components.Parameters = oas3.ParametersMap{}
+				}
+				destSpec.Components.Parameters[paramName] = paramRef
+			}
+		}
+		_, isSchema := ptr.IsTopSchema()
+		if isSchema {
+			err := sm.SchemasCopyJSONPointer(destSpec, refJSONPointer)
+			if err != nil {
+				return err
+			}
+		}
+		if !isParam && !isSchema {
+			return errors.New("pointer is not components/parameters or components/schemas")
+		}
+	}
+	return nil
+}
+
+func (sm *SpecMore) SchemasCopySchemaRef(destSpec *Spec, schRef *oas3.SchemaRef) error {
+	if sm.Spec == nil || destSpec == nil || schRef == nil {
+		return nil
+	}
+	if len(strings.TrimSpace(schRef.Ref)) > 0 {
+		sm.SchemasCopyJSONPointer(destSpec, schRef.Ref)
+	}
+	if schRef.Value == nil {
+		return nil
+	}
+	if schRef.Value.Items != nil {
+		sm.SchemasCopySchemaRef(destSpec, schRef.Value.Items)
+	}
+	for _, schRefProp := range schRef.Value.Properties {
+		err := sm.SchemasCopySchemaRef(destSpec, schRefProp)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (sm *SpecMore) SchemasCopyJSONPointer(destSpec *Spec, jsonPointer string) error {
+	ptr, err := ParseJSONPointer(jsonPointer)
+	if err != nil {
+		return err
+	}
+	schName, ok := ptr.IsTopSchema()
+	if !ok {
+		return errors.New("json pointer is not schema pointer")
+	}
+	destSM := SpecMore{Spec: destSpec}
+	destSchRef := destSM.SchemaRef(schName)
+	if destSchRef != nil { // already present.
+		return nil
+	}
+	srcSchRef := sm.SchemaRef(schName)
+	if srcSchRef == nil {
+		return fmt.Errorf("json pointer not found [%s][%s]", jsonPointer, schName)
+	}
+	err = destSM.SchemaRefSet(schName, srcSchRef)
+	if err != nil {
+		return err
+	}
+	// Check recursive.
+	return sm.SchemasCopySchemaRef(destSpec, srcSchRef)
 }
