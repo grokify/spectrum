30bde8a8ea217bb24f64366e2605f17256651b7c
John Wang <johncwang@gmail.com>
2022-12-14 19:37:56 -0800

    feat: `openapi3edit`; add `NewSpecEdit()`

--- openapi3edit/spec_edit.go
+++ openapi3edit/spec_edit.go
@@ -6,6 +6,11 @@ type SpecEdit struct {
 	SpecMore openapi3.SpecMore
 }
 
+func NewSpecEdit(spec *openapi3.Spec) SpecEdit {
+	return SpecEdit{
+		SpecMore: openapi3.SpecMore{Spec: spec}}
+}
+
 func (se *SpecEdit) ExtensionSet(key string, val any) {
 	se.SpecMore.Spec.ExtensionProps.Extensions[key] = val
 }
